e22faec39f61ae5bf6185a889701b1d0d3dd52a5
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-17 11:40:26 +0530

    Locators in Playwright

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -1,12 +1,27 @@
-import { test,expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test('Locators in Playwright',async({page})=>{
+test('Locators in Playwright', async ({ page }) => {
 
+   const userName = page.locator('#user-name');
+   const passWord = page.locator('#password');
+   const clickBtn = page.locator('#login-button');
+
+   // Enter the Value in Fields 
    await page.goto("https://www.saucedemo.com/");
    console.log(await page.title());
-   await page.locator('#user-name').type('Naresh Chauhan');
-   await page.locator('#password').type('12345');
-   await page.locator('#login-button').click;
+   await userName.type('Naresh Chauhan');
+   await passWord.type('12345');
+   // Click the Login Butten 
+   await clickBtn.click;
+
+   // Again Enter the Value after Removing the Exsiting one
+
+   await userName.clear();
+   await userName.fill("Kapil")
+   await passWord.clear();
+   await passWord.fill('9876543');
+   await clickBtn.click;
+
 
 })
 
