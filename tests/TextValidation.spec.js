1d3514122f0c9094e6865d4e1c8c4f0b1bef345c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-17 12:42:48 +0530

    Report

--- tests/TextValidation.spec.js
+++ tests/TextValidation.spec.js
@@ -2,14 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test('Locators in Playwright', async ({ page }) => {
 
-   const userName = page.locator('#user-name');
-   const passWord = page.locator('#password');
-   const clickBtn = page.locator('#login-button');
+   let userName = page.locator('#user-name');
+   let passWord = page.locator('#password');
 
    await page.goto("https://www.saucedemo.com/");
    await userName.fill("standard_user")
    await passWord.fill('secret_sauce');
-   await clickBtn.click;
+   await page.locator('#login-button').click();
 
    let text =await page.locator("a[id='item_4_title_link'] div[class='inventory_item_name']").textContent();
 
