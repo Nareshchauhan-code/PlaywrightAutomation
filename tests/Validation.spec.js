359bb442f637833c803b3edb890397b329c87ed9
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-17 16:33:43 +0530

    orangehrmlive.com

--- tests/Validation.spec.js
+++ tests/Validation.spec.js
@@ -2,15 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test('Validation', async ({ page }) => {
 
+    const userName = page.locator("[name='username']");
+    const password = page.locator("[name='password']");
+    const submitbtn = page.locator("[type='submit']");
+    const admitbtn = page.locator("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1) > span:nth-child(2)");
+    const addbtn = page.locator("button[class='oxd-button oxd-button--medium oxd-button--secondary']");
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator("[name='username']").type("Admin");
-    await page.locator("[name='password']").type("admin123");
-    await page.locator("[type='submit']").click();
+    await userName.type("Admin");
+    await password.type("admin123");
+    await submitbtn.click();
 
-    await page.locator(".oxd-main-menu-item.active").click();
-
-    await page.locator("button[class='oxd-button oxd-button--medium oxd-button--secondary']").click();
+    await admitbtn.click();
+    await addbtn.click();
 
 })
