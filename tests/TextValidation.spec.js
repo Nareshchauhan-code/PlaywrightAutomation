765e107d87a2330a49edf3ca8188bf2381a25a00
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-17 12:03:03 +0530

    Css Selector

--- tests/TextValidation.spec.js
+++ tests/TextValidation.spec.js
@@ -6,21 +6,14 @@ test('Locators in Playwright', async ({ page }) => {
    const passWord = page.locator('#password');
    const clickBtn = page.locator('#login-button');
 
-   // Enter the Value in Fields 
    await page.goto("https://www.saucedemo.com/");
-   console.log(await page.title());
-   await userName.type('Naresh Chauhan');
-   await passWord.type('12345');
-   // Click the Login Butten 
-   await clickBtn.click;
-
-   // Again Enter the Value after Removing the Exsiting one
-
-   await userName.clear();
    await userName.fill("standard_user")
-   await passWord.clear();
    await passWord.fill('secret_sauce');
    await clickBtn.click;
+
+   let text =await page.locator("a[id='item_4_title_link'] div[class='inventory_item_name']").textContent();
+
+   console.log(text);
 
 
 })
