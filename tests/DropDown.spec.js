d14d59fb76f6cbed4c4e1dcf40b513d44baa410f
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-18 01:24:07 +0530

    Check Box

--- tests/DropDown.spec.js
+++ tests/DropDown.spec.js
@@ -8,13 +8,18 @@ test('DropDown Test', async ({ page }) => {
     await page.locator("#password").type("learning");
     await page.locator("span.checkmark").last().click();
 
-    expect(await page.locator("span.checkmark").last()).toBeChecked();
+    await expect(page.locator("span.checkmark").last()).toBeChecked();
     console.log(await page.locator("span.checkmark").last().isChecked());
-    expect(await page.locator("span.checkmark").last()).toBeTruthy();
+    await expect(page.locator("span.checkmark").last()).toBeTruthy();
     await page.locator("#okayBtn").click();
 
     await page.locator("select.form-control").selectOption('consult');
+    await page.locator("#terms").click();
+    await page.locator("#terms").uncheck();
+    await page.locator("#terms").click();
 
-    //await page.pause();
+    await page.locator("#signInBtn").click();
+
+    await page.pause();
 
 })
