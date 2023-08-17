baabf90bbc1f2d25dfadffe9fc4bacdeea91bae4
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-17 17:25:15 +0530

    Validation

--- tests/Validation.spec.js
+++ tests/Validation.spec.js
@@ -5,7 +5,7 @@ test('Validation', async ({ page }) => {
     const userName = page.locator("[name='username']");
     const password = page.locator("[name='password']");
     const submitbtn = page.locator("[type='submit']");
-    const admitbtn = page.locator("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1) > span:nth-child(2)");
+    const admintbtn = page.locator("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1) > span:nth-child(2)");
     const addbtn = page.locator("button[class='oxd-button oxd-button--medium oxd-button--secondary']");
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
@@ -14,7 +14,7 @@ test('Validation', async ({ page }) => {
     await password.type("admin123");
     await submitbtn.click();
 
-    await admitbtn.click();
+    await admintbtn.click();
     await addbtn.click();
 
 })
