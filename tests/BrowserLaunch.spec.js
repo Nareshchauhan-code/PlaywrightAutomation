8b68665488aa7249412a7c8f7645cd8395a43827
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-20 00:18:54 +0530

    Browser Launch

--- tests/BrowserLaunch.spec.js
+++ tests/BrowserLaunch.spec.js
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test'
-
-test("Browser Launch", async ({ browser }) => {
+test('Browser Launch', async ({ browser }) => {
 
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto('https://facebook.com');
-
+    await page.goto('https://google.com');
+    await page.locator('#APjFqb').fill('Welcome to playwright !!!');
+    await page.pause();
 
 })
