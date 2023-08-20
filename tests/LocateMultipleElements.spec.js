75c28b2b851e36db8b3767dc3fe0426ce0dcc145
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-20 16:37:47 +0530

    Multiple Elements Locate

--- tests/LocateMultipleElements.spec.js
+++ tests/LocateMultipleElements.spec.js
@@ -1,15 +1,33 @@
 import { test, expect } from '@playwright/test'
 
-test('Multiple Element Locate', async ({ page }) => {
+test('Multiple Links Locate', async ({ page }) => {
 
     await page.goto('https://demoblaze.com/');
     const links = await page.$$('a');
 
-    for(const link of links ){
+    for (const link of links) {
 
-       const text =await link.textContent();
+        const text = await link.textContent();
 
-       console.log(text);
+        console.log(text);
+    }
+
+})
+
+test('Multiple Elements Locate', async ({ page }) => {
+
+    await page.goto('https://demoblaze.com/');
+
+    page.waitForSelector("//div[@id='tbodyid']//div//h4/a")
+
+    const products = await page.$$("//div[@id='tbodyid']//div//h4/a");
+
+    for (const product of products) {
+
+        const productName = await product.textContent();
+
+        console.log("Product Name is : ", productName)
+
     }
 
 })
