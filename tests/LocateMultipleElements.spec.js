2ed9ce0c28c39894c7e7d738c388f0c5fa56b128
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-21 14:09:32 +0530

    Locators

--- tests/LocateMultipleElements.spec.js
+++ tests/LocateMultipleElements.spec.js
@@ -1,22 +1,5 @@
 import { test, expect } from '@playwright/test'
 
-// let context
-// let page
-
-// test.beforeAll(async ({ browser }) => {
-
-//     context = await browser.newContext();
-//     await context.tracing.start({ screenshots: true, snapshots: true })
-//     page = await context.newPage();
-
-// })
-
-// test.afterAll(async () => {
-
-//     await context.tracing.stop({ path: '.\PlaywrightAutomation\test-results\locator.trace.zip' })
-
-// })
-
 test('Multiple Links Locate', async ({ page }) => {
 
     await page.goto('https://demoblaze.com/');
