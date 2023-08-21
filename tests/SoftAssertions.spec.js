9b3fa22e7b61b6751caf909dc4fc56615b1824ba
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-22 01:43:21 +0530

    Soft Assertions

--- tests/SoftAssertions.spec.js
+++ tests/SoftAssertions.spec.js
@@ -3,9 +3,7 @@ import { test, expect } from '@playwright/test'
 test('Soft Assertions', async ({ page }) => {
 
     await page.goto('https://demo.nopcommerce.com/register');
-
     await expect.soft(page).toHaveTitle('nopCommerce demo store. Register');
-
     await expect.soft(page).toHaveURL('https://demo.nopcommerce.com/register');
 
 })
