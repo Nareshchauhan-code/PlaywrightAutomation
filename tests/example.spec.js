8498ab3a9f62defd14f969a5d2e079311bcfdf2f
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-21 11:48:09 +0530

    Trace Viewer

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,23 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+let context
+let page
+
+test.beforeAll(async ({ browser }) => {
+
+    context = await browser.newContext();
+    await context.tracing.start({ screenshots: true, snapshots: true })
+    page = await context.newPage();
+
+})
+
+test.afterAll(async () => {
+
+    await context.tracing.stop({ path: '.\PlaywrightAutomation\test-results\locator.trace.zip' })
+
+})
+
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
