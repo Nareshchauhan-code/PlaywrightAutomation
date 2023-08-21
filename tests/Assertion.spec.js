13e4a33a5bb490b6ae910d9af2d13f059f692c76
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-22 01:01:43 +0530

    Locator

--- tests/Assertion.spec.js
+++ tests/Assertion.spec.js
@@ -5,15 +5,12 @@ test('Assertions ', async ({ page }) => {
     await page.goto('https://demo.nopcommerce.com/register');
 
     //toHaveURL()
-
     await expect(page).toHaveURL('https://demo.nopcommerce.com/register');
-
     const title = await page.title();
     console.log(title);
 
     //toHaveTitle()
     await expect(page).toHaveTitle(title);
-
     const logo = await page.locator(".header-logo");
 
     // toBeVisible()
@@ -21,8 +18,15 @@ test('Assertions ', async ({ page }) => {
 
     //toBeEnabled()
     const searchBox = await page.locator("#small-searchterms");
-
     await expect(searchBox).toBeEnabled();
+
+    // toBeChecked()
+    const radiobtn = await page.locator("#gender-male");
+    radiobtn.click();
+    await expect(radiobtn).toBeChecked();
+    const checkBox = await page.locator("#Newsletter").first();
+    await expect(checkBox).toBeChecked();
+    await page.pause();
 
 
 
