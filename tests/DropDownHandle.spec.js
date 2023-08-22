d3c88c239b5c862593752fbc5aa5c1341ff3d3a6
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-22 15:13:34 +0530

    Drop Down Handle

--- tests/DropDownHandle.spec.js
+++ tests/DropDownHandle.spec.js
@@ -9,7 +9,24 @@ test('Drop Down Handle', async ({ page }) => {
     //await page.locator("//select[@id='country' and @class='form-control']").selectOption({ label: 'India' });
 
     // Visible Text
-    await page.locator("//select[@id='country' and @class='form-control']").selectOption('India');
+    //await page.locator("//select[@id='country' and @class='form-control']").selectOption('India');
+
+    // Select by Value
+
+    //await page.locator("//select[@id='country' and @class='form-control']").selectOption({value:'usa'});
+
+    // Select by Index 
+
+    await page.locator("//select[@id='country' and @class='form-control']").selectOption({ index: 1 });
+
+    // Find the all Options 1st way
+    const drpOption = await page.locator("//select[@id='country' and @class='form-control']/option");
+    await expect(drpOption).toHaveCount(10);
+    // 2nd way
+
+    const allOptions = await page.$$("//select[@id='country' and @class='form-control']/option");
+    console.log("Total Options : ", await allOptions.length)
+
 
     await page.waitForTimeout(3000);
 
