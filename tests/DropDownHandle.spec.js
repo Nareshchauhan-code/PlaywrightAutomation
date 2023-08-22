59e9a0eab717a448b895832950551f0af1d9dd97
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-22 14:51:07 +0530

    Drop Down Handle

--- tests/DropDownHandle.spec.js
+++ tests/DropDownHandle.spec.js
@@ -5,7 +5,11 @@ test('Drop Down Handle', async ({ page }) => {
 
     await page.goto('https://testautomationpractice.blogspot.com/');
 
-    await page.locator("//select[@id='country' and @class='form-control']").selectOption({ label: 'India' });
+    //Label
+    //await page.locator("//select[@id='country' and @class='form-control']").selectOption({ label: 'India' });
+
+    // Visible Text
+    await page.locator("//select[@id='country' and @class='form-control']").selectOption('India');
 
     await page.waitForTimeout(3000);
 
