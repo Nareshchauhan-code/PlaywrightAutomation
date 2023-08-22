f94e86f029dfa3520b0961e9873e1c327481b0f4
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-22 12:53:44 +0530

    Drop Down Handle

---
+++ tests/DropDownHandle.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test'
+
+test('Drop Down Handle', async ({ page }) => {
+
+
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    
+
+
+})
